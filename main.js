c8206a9b7ab815c017b64ecc7321957cff255a8b
AbdujalilovHasan <abdujalilovhasan171@gmaile.com>
2024-06-05 15:10:34 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+// 1# 
+
+// let num_1 = 12;
+// let num_2 = 8;
+
+// console.log(num_1 + num_2);
+
+
+
+// 2#
+
+// let a = "12"
+// let b = +a
+
+// console.log(typeof(b))
+
+
+
+// 3# 
+
+// let randomNumber = Math.floor(Math.random() * 100);
+
+// if (randomNumber % 2 === 0) {
+//     console.log(randomNumber + " juft son.");
+// } else {
+//     console.log(randomNumber + " toq son.");
+// }
+
+
+
+// 4#
+
+// let a = prompt("Iltimos, bitta so'z kiriting:");
+
+// if (a === "Mars") {
+//     console.log(a);
+// } else {
+//     console.log("Mars It school");
+// }
+
+
+
+//5#
+
+// let texts = ["apple", "banana", "cat", "dolphin", "elephant"];
+// let newText =[]
+
+// for (let i = 0; i < texts.length; i++) {
+//    if(texts[i].length >= 6){
+//     newText.push(texts[i])
+//    }
+    
+// }
+// console.log(newText);
+
+
+
+// 6#
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i + " - Juft");
+//     } else {
+//         console.log(i + " - Toq");
+//     }
+// }
